a9c87a9690dcf34e06d8aefc529067d51fafd18c
@hexlet/cli <support@hexlet.io>
2022-12-05 16:21:11 +0300

    submit java-web-ru/jdbc

---
+++ java-web-ru/jdbc/src/main/java/exercise/servlet/ArticlesServlet.java
@@ -0,0 +1,136 @@
+package exercise.servlet;
+
+import exercise.TemplateEngineUtil;
+import org.apache.commons.lang3.ArrayUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+
+public class ArticlesServlet extends HttpServlet {
+
+    private String getId(HttpServletRequest request) {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            return null;
+        }
+        String[] pathParts = pathInfo.split("/");
+        return ArrayUtils.get(pathParts, 1, null);
+    }
+
+    private String getAction(HttpServletRequest request) {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            return "list";
+        }
+        String[] pathParts = pathInfo.split("/");
+        return ArrayUtils.get(pathParts, 2, getId(request));
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+
+        String action = getAction(request);
+
+        switch (action) {
+            case "list":
+                showArticles(request, response);
+                break;
+            default:
+                showArticle(request, response);
+                break;
+        }
+    }
+
+    private void showArticles(HttpServletRequest request,
+                              HttpServletResponse response)
+            throws IOException, ServletException {
+
+        ServletContext context = request.getServletContext();
+        Connection connection = (Connection) context.getAttribute("dbConnection");
+        // BEGIN
+        String currentPage = "";
+        if (request.getParameter("page") == null) {
+            currentPage = "1";
+        } else {
+            currentPage = request.getParameter("page");
+        }
+        int offset = 0;
+        if (currentPage != null) {
+            offset = Integer.parseInt(currentPage) * 10 - 10;
+        }
+        List<Map<String, String>> articles = new ArrayList<>();
+        String query = "SELECT * FROM articles ORDER BY id LIMIT 10 OFFSET ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, Integer.toString(offset));
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                articles.add(Map.of(
+                                "id", rs.getString("id"),
+                                "title", rs.getString("title"),
+                                "body", rs.getString("body")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+        request.setAttribute("articles", articles);
+        request.setAttribute("currentPage", currentPage);
+        // END
+        TemplateEngineUtil.render("articles/index.html", request, response);
+    }
+
+    private void showArticle(HttpServletRequest request,
+                             HttpServletResponse response)
+            throws IOException, ServletException {
+
+        ServletContext context = request.getServletContext();
+        Connection connection = (Connection) context.getAttribute("dbConnection");
+        // BEGIN
+        String numberOfArticle = Objects.requireNonNull(getId(request));
+        Map<String, String> article = new HashMap<>();
+        String query = "SELECT * FROM articles WHERE id=?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, numberOfArticle);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                article.putAll(Map.of(
+                                "id", rs.getString("id"),
+                                "title", rs.getString("title"),
+                                "body", rs.getString("body")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+        if (article.isEmpty()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        } else {
+            request.setAttribute("article", article);
+        }
+        // END
+        TemplateEngineUtil.render("articles/show.html", request, response);
+    }
+}
